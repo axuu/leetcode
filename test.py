c7aafc3459fa3a62220717ff3789f28a52e54018
axuu <zhangxu.fred@gmail.com>
2023-11-04 21:00:00 +0800

    add problems

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 class Solution:
-    def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:  
-        if (m == 0 or n == 0):
-            return
-        for target in nums2:
-            print(target)
-            for i in range(m + n - 1, -1, -1):
-                if (target > nums1[i] and nums1[i+1] == 0):
-                    nums1[i+1] = target
-                    break
-                if (target >= nums1[i-1] and target < nums1[i]):
-                    nums1[i] = target
-                    break
-                else:
-                    nums1[i] = nums1[i - 1]
-                print(nums1)
-
-        return nums1
+    def majorityElement(nums: list[int]) -> int:
+        count = 0
+        candidate = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+            print('candidate', candidate)
+            print('count', count)
+            print('----------')
+        
+        return candidate
     
-print(Solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3))
+Solution.majorityElement([2,2,1,1,1,1,1,2,3])
