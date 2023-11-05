a9cf2dba213a14ead3269887946121aabbb5f003
axuu <zhangxu.fred@gmail.com>
2023-11-05 21:00:00 +0800

    update

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 class Solution:
-    def removeElement(nums: list[int], val: int) -> int:
-        if(len(nums) == 1):
-            if(nums[0] == val):
-                return 0
-            else:
-                return 1
+    def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:  
+        if (m == 0 or n == 0):
+            return
+        for target in nums2:
+            print(target)
+            for i in range(m + n - 1, -1, -1):
+                if (target > nums1[i] and nums1[i+1] == 0):
+                    nums1[i+1] = target
+                    break
+                if (target >= nums1[i-1] and target < nums1[i]):
+                    nums1[i] = target
+                    break
+                else:
+                    nums1[i] = nums1[i - 1]
+                print(nums1)
 
-        i = 0
-        j = len(nums) - 1
-        while i <= j:
-            if nums[i] == val:
-                nums[i] = nums[j]
-                nums[j] = "_"
-                j = j - 1
-            else:
-                i = i + 1
-        return j + 1
-
+        return nums1
     
-print(Solution.removeElement([3,3], 3))
+print(Solution.merge([1,2,3,0,0,0], 3, [2,5,6], 3))
