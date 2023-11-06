47eaa78235c98931bddaa2075b3d69ef8e74d706
axuu <zhangxu.fred@gmail.com>
2023-11-06 20:17:18 +0800

    update problems

---
+++ 0014_Longest_Common_Prefix.md
@@ -0,0 +1,20 @@
+## 思考
+
+本题有几个难点, 一个是利用自带函数 sorted 对字符串数组进行排序, 然后比较最长的字符串和最短的字符串即可.
+第二个是边缘情况比较难处理, 比如[""], ["", ""] 等情况, 如果用切片的方式如 str[:i] 来处理, 会出现部分 case 不能通过的情况.
+
+## 题解
+
+```python
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        strs = sorted(strs)
+        first = strs[0]
+        last = strs[-1]
+        result = ''
+        for i in range(len(strs[0])):
+            if (first[i] != last[i]):
+                break
+            result += first[i]
+        return result
+```
--- test.py
+++ test.py
@@ -1,32 +1,15 @@
 class Solution:
-    def romanToInt(s: str) -> int:
-        map1 = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000,
-        }
-        map2 = {
-            'IV': 4,
-            'IX': 9,
-            'XL': 40,
-            'XC': 90,
-            'CD': 400,
-            'CM': 900,
-        }
-        i = 0
-        result = 0
-        while i < len(s):
-            if(i < len(s) - 1 and map2.get(s[i] + s[i+1])):
-                result += map2[s[i] + s[i+1]]
-                i += 2
-            else:
-                result += map1[s[i]]
-                i += 1
-        print(result)
+    def longestCommonPrefix(strs: list[str]) -> str:
+        if (len(strs) == 1):
+            return strs[0]
+        strs = sorted(strs)
+        first = strs[0]
+        last = strs[-1]
+        result = ''
+        for i in range(len(strs[0])):
+            if (first[i] != last[i]):
+                break
+            result += first[i]
         return result
     
-Solution.romanToInt("MCDLXXVI")
+print(Solution.longestCommonPrefix(["",""]))
